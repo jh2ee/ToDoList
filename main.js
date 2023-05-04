eed0378bce6c95316c6e06e727ff202fa030d2fc
jh2ee <ljh20000321@gmail.com>
2023-05-04 17:40:02 +0900

    dev1

--- main.js
+++ main.js
@@ -6,17 +6,27 @@ function addtodo(event){
     event.preventDefault();
     if(input.value!=""){
         const list=document.createElement('div');
+        list.id='listitem';
+        //체크박스
         const checkBox=document.createElement('input');
         checkBox.type="checkbox";
         checkBox.id='cb';
+        //체크박스 꾸미기 위한 라벨
         const checkBoxLabel=document.createElement('LABEL');
         checkBoxLabel.htmlFor='cb';
+        //텍스트
         const text=document.createElement('span');
-
+        //삭제버튼
+        const deleteButton = document.createElement('button')
+        deleteButton.id='del';
+        deleteButton.textContent="delete"
+        //텍스트 입력
         text.innerText=input.value;
+        //리스트 추가
         list.appendChild(checkBox);
         list.appendChild(checkBoxLabel);
         list.appendChild(text);
+        list.appendChild(deleteButton);
         todolist.appendChild(list);
 
         checkBox.addEventListener('change', (event) =>{ 
@@ -29,11 +39,16 @@ function addtodo(event){
             }
         })
 
+        deleteButton.addEventListener('click', (event) => {
+            todolist.removeChild(event.currentTarget.parentNode)
+        })
+
     }
     else{
         
     }
     input.value="";
 }
+
 
 add.addEventListener("click", addtodo);
